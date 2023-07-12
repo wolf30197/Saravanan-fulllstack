99bf447fa5fa6d368d32395e624dbd50950cd2ed
Saravanan-S11 <saravananbtechit11@gmail.com>
2023-07-12 20:09:47 +0530

    "resolve"

--- Style/JavaScript/abc.js
+++ Style/JavaScript/abc.js
@@ -326,3 +326,4 @@ array1=[1,2,3,4,5,6,7,8,9,10];
             console.log('Even',array1[j]);
         }
     }
+    
--- Style/JavaScript/api.js
+++
@@ -1,5 +0,0 @@
-async function getData(){
-    let response= await fetch('https://reqres.in/api/users?page-1');
-    let data= await response.json();
-    console.log(data);
-}
